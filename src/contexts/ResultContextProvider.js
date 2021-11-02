1e149b0a5dafeaea5345fa1abcf9f2da148d4e26
Harshit Kumar <kumarharshit850@gmail.com>
2021-11-02 15:34:45 +0530

    All complete

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -5,23 +5,31 @@ const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 
 export const ResultContextProvider = ({ children }) => {
   const [results, setResults] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('Elon Musk');
   const [isLoading, setIsLoading] = useState(false);
 
   // /videos, /search, /images
   const getResults = async (type) => {
     setIsLoading(true);
+
     const response = await fetch(`${baseUrl}${type}`, {
       method: 'GET',
-      header: {
+      headers: {
         'x-user-agent': 'desktop',
         'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-        'x-rapidapi-key': '65fe158fb3msh4bd83b607be502cp17f2e5jsn1f1d21c83850',
+        'x-rapidapi-key': process.env.REACT_APP_API_KEY,
       },
     });
 
     const data = await response.json();
-    setResults(data);
+
+    if (type.includes('/news')) {
+      setResults(data.entries);
+    } else if (type.includes('/images')) {
+      setResults(data.image_results);
+    } else {
+      setResults(data.results);
+    }
     setIsLoading(false);
   };
 
